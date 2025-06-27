c432be71f186f34379329bfe4391d1213b97c325
Anshusingh Rajput <rajputanshu706@gmail.com>
2025-06-27 15:48:33 +0530

    Updated Yaml flow

--- src/main/java/framework/drivers/DriverManager.java
+++ src/main/java/framework/drivers/DriverManager.java
@@ -4,8 +4,10 @@ import framework.config.ConfigReader;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class DriverManager {
     private static final ThreadLocal<WebDriver> driver = new ThreadLocal<>();
@@ -15,24 +17,38 @@ public class DriverManager {
 
         if (browser == null || browser.isEmpty()) {
             browser = ConfigReader.get("browser");
-            System.out.println("Browser not passed via CLI:" + browser);
+            System.out.println("Browser not passed via CLI: " + browser);
         } else {
             System.out.println("Browser passed from CLI: " + browser);
         }
 
         switch (browser.toLowerCase()) {
             case "chrome":
-                WebDriverManager.chromedriver().setup();  
-                driver.set(new ChromeDriver());
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--headless=new");
+                chromeOptions.addArguments("--no-sandbox");
+                chromeOptions.addArguments("--disable-dev-shm-usage");
+                chromeOptions.addArguments("--disable-gpu");
+                chromeOptions.addArguments("--window-size=1920,1080");
+                chromeOptions.addArguments("--user-data-dir=/tmp/chrome-profile-" + System.currentTimeMillis());
+                driver.set(new ChromeDriver(chromeOptions));
                 break;
+
             case "firefox":
-                WebDriverManager.firefoxdriver().setup(); 
-                driver.set(new FirefoxDriver());
+                WebDriverManager.firefoxdriver().setup();
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                firefoxOptions.addArguments("--headless");
+                firefoxOptions.addArguments("--width=1920");
+                firefoxOptions.addArguments("--height=1080");
+                driver.set(new FirefoxDriver(firefoxOptions));
                 break;
+
             case "edge":
                 WebDriverManager.edgedriver().setup();
                 driver.set(new EdgeDriver());
                 break;
+
             default:
                 throw new RuntimeException("❌ Unsupported browser: " + browser);
         }
